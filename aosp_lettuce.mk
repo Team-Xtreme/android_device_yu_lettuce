2d435714ac91e8549dc9cc2c7b2f15b142e991b5
Rpingat <pingatravi@gmail.com>
2018-01-24 20:58:06 +0530

    Pixel Experience Bringup
    Change-Id: I049e350f817eda65466d6c803fffbe0b22479db0

--- aosp_lettuce.mk
+++ aosp_lettuce.mk
@@ -14,13 +14,16 @@
 
 $(call inherit-product, device/yu/lettuce/full_lettuce.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xhdpi
+IS_PHONE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_lettuce
+PRODUCT_NAME := aosp_lettuce
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := lettuce
 
