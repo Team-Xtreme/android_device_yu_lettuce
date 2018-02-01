d603bf1b4aebebc450c22de01c2bde042f8a6bec
Ravi Pingat <31516273+Rpingat@users.noreply.github.com>
2018-02-01 16:37:28 +0530

    Switch to Linaro 7.2

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,3 +5,5 @@ BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_lettuce_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linaro-7.2/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
